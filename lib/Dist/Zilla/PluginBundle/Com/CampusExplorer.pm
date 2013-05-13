07867ee3cfd806699591d381f06cc133c9b28ba7
Chris Weyl <cweyl@alumni.drew.edu>
2013-05-13 14:52:08 -0700

    initial commit
    Totally cheating here.  As I'm the only one using this at the moment, so we
    simply extend the RSRCHBOY bundle.
    
    This sets the stage, at least :)

--- lib/Dist/Zilla/PluginBundle/Com/CampusExplorer.pm
+++ lib/Dist/Zilla/PluginBundle/Com/CampusExplorer.pm
@@ -1,10 +1,11 @@
 package Dist::Zilla::PluginBundle::Com::CampusExplorer;
 
-# ABSTRACT: The great new Dist::Zilla::PluginBundle::Com::CampusExplorer!
+# ABSTRACT: Dist::Zilla plugin bundle for C<campusexplorer.com>
 
 use Moose;
 use namespace::autoclean;
 
+extends 'Dist::Zilla::PluginBundle::RSRCHBOY';
 
 __PACKAGE__->meta->make_immutable;
 !!42;
@@ -12,8 +13,13 @@ __END__
 
 =head1 SYNOPSIS
 
+    # in your dist.ini:
+    [@Com::CampusExplorer]
+
 =head1 DESCRIPTION
 
-=head1 SEE ALSO
+This is a L<Dist::Zilla> author bundle for
+L<Campus Explorer|http://campusexplorer.com>.  It's intended to help maintain
+consistency across our open sourced projects.
 
 =cut
