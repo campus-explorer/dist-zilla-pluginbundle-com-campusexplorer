cdf4be197585558c266dda3d3015ac0d3063255f
Chris Weyl <cweyl@alumni.drew.edu>
2013-05-13 14:47:41 -0700

    initial commit

---
+++ lib/Dist/Zilla/PluginBundle/Com/CampusExplorer.pm
@@ -0,0 +1,19 @@
+package Dist::Zilla::PluginBundle::Com::CampusExplorer;
+
+# ABSTRACT: The great new Dist::Zilla::PluginBundle::Com::CampusExplorer!
+
+use Moose;
+use namespace::autoclean;
+
+
+__PACKAGE__->meta->make_immutable;
+!!42;
+__END__
+
+=head1 SYNOPSIS
+
+=head1 DESCRIPTION
+
+=head1 SEE ALSO
+
+=cut
